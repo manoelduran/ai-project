0fbfcc030f4871098846ae5406d5e0e21c9021b4
Manoel Cendon Duran <manoel.duran@hotmail.com>
2024-09-29 22:09:35 -0300

    initial commit

---
+++ scrapper.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_text_from_url(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        for script in soup(["script", "style"]):
+            script.extract()
+
+        text = soup.get_text()
+        lines = (line.strip() for line in text.splitlines())
+        chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+        text = '\n'.join(chunk for chunk in chunks if chunk)
+        return text
+    else:
+        return f"Error: {response.status_code}"
